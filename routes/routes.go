eb9a1a00c81d7884babe433afada059f7a5ff211
Helmbearer <eiseneber666@gmx.de>
2024-06-02 23:11:05 +0200

    added routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func RegisterRoutes() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/users", controllers.GetAllUsers).Methods("GET")
+	router.HandleFunc("/users".controllers.CreateUser).Methods("POST")
+	router.HandleFunc("/users/{id}", controllers.GetUserById).Methods("GET")
+	router.HandleFunc("/users/{id}", controllers.UpdateUser).Methods("Put")
+	router.HandleFunc("/users/{id}", controllers.DeleteUser).Methods("DELETE")
+	return router
+}

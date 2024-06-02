f9da4730dabb5be5459e054cb8a4e48d69029924
Helmbearer <eiseneber666@gmx.de>
2024-06-02 23:18:48 +0200

    fixed imports

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,14 @@
 package routes
 
 import (
+	"SIMPLE-API/controllers"
 	"github.com/gorilla/mux"
 )
 
 func RegisterRoutes() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/users", controllers.GetAllUsers).Methods("GET")
-	router.HandleFunc("/users".controllers.CreateUser).Methods("POST")
+	router.HandleFunc("/users", controllers.CreateUser).Methods("POST")
 	router.HandleFunc("/users/{id}", controllers.GetUserById).Methods("GET")
 	router.HandleFunc("/users/{id}", controllers.UpdateUser).Methods("Put")
 	router.HandleFunc("/users/{id}", controllers.DeleteUser).Methods("DELETE")

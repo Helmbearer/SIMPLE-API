ca9e94c5964616e5fa04e0f84b677e5196a3c653
Helmbearer <eiseneber666@gmx.de>
2024-06-02 23:16:20 +0200

    implemented connection to database

--- config/db.go
+++ config/db.go
@@ -1,1 +1,20 @@
 package config
+
+import (
+	"SIMPLE-API/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.AutoMigrate(&models.User{})
+
+	DB = database
+}

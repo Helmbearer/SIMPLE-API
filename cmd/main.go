79e857883f660a9eee9694b8634b6bd7d610c332
Helmbearer <eiseneber666@gmx.de>
2024-06-02 23:17:14 +0200

    added entry point

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
-package cmd
+package main
+
+import (
+	"SIMPLE-API/config"
+	"SIMPLE-API/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.ConnectDB()
+
+	router := routes.RegisterRoutes()
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
